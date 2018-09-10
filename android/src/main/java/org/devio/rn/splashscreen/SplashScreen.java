0f1b548609499f64e61579b5bf71eb27803f04a3
mohanraj <mohanraj.kandregula@gmail.com>
2018-09-10 11:15:29 +0530

    updated the build tools and added resources

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -2,7 +2,8 @@ package org.devio.rn.splashscreen;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.os.Build;
+import android.content.Context;
+import android.graphics.Typeface;
 
 import java.lang.ref.WeakReference;
 
@@ -21,15 +22,19 @@ public class SplashScreen {
     /**
      * 打开启动屏
      */
-    public static void show(final Activity activity, final int themeResId) {
+    public static void show(final Activity activity, final boolean fullScreen) {
         if (activity == null) return;
         mActivity = new WeakReference<Activity>(activity);
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 if (!activity.isFinishing()) {
-                    mSplashDialog = new Dialog(activity, themeResId);
+
+                    mSplashDialog = new Dialog(activity, fullScreen ? R.style.SplashScreen_Fullscreen : R.style.SplashScreen_SplashTheme);
                     mSplashDialog.setContentView(R.layout.launch_screen);
+
+                    Typeface face = Typeface.createFromAsset(activity.getAssets(),
+                            "fonts/ProximaNova-Regular.ttf");
                     mSplashDialog.setCancelable(false);
 
                     if (!mSplashDialog.isShowing()) {
@@ -38,15 +43,6 @@ public class SplashScreen {
                 }
             }
         });
-    }
-
-    /**
-     * 打开启动屏
-     */
-    public static void show(final Activity activity, final boolean fullScreen) {
-        int resourceId = fullScreen ? R.style.SplashScreen_Fullscreen : R.style.SplashScreen_SplashTheme;
-
-        show(activity, resourceId);
     }
 
     /**
@@ -66,24 +62,13 @@ public class SplashScreen {
             }
             activity = mActivity.get();
         }
-
         if (activity == null) return;
 
-        final Activity _activity = activity;
-
-        _activity.runOnUiThread(new Runnable() {
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 if (mSplashDialog != null && mSplashDialog.isShowing()) {
-                    boolean isDestroyed = false;
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                        isDestroyed = _activity.isDestroyed();
-                    }
-
-                    if (!_activity.isFinishing() && !isDestroyed) {
-                        mSplashDialog.dismiss();
-                    }
+                    mSplashDialog.dismiss();
                     mSplashDialog = null;
                 }
             }
